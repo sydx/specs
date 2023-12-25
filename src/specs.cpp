3aaef9d1eabbe4a19748a6cd2ebbd8ca04c3aaa1
Paul <paul@thalesians.com>
2023-12-25 12:50:48 +0100

    Tweaked debug output.

--- src/specs.cpp
+++ src/specs.cpp
@@ -481,16 +481,23 @@ Rcpp::List specs_rcpp (const arma::vec y,const arma::mat x,const int p,
     ret["D"] = D;
     std::cout << "(Collected output in list)" << std::endl;
 
-    std::cout << "ret[gammas]: " << ret["gammas"] << std::endl;
-    std::cout << "ret[thetas]: " << ret["thetas"] << std::endl;
-    std::cout << "ret[lambda_i]: " << ret["lambda_i"] << std::endl;
-    std::cout << "ret[lambda_g]: " << ret["lambda_g"] << std::endl;
-    std::cout << "ret[weights]: " << ret["weights"] << std::endl;
-    std::cout << "ret[y_d]: " << ret["y_d"] << std::endl;
-    std::cout << "ret[My_d]: " << ret["My_d"] << std::endl;
-    std::cout << "ret[v]: " << ret["v"] << std::endl;
-    std::cout << "ret[Mv]: " << ret["Mv"] << std::endl;
-    std::cout << "ret[D]: " << ret["D"] << std::endl;
+    std::cout << "ret[gammas]: " << gammas << std::endl;
+    std::cout << "ret[thetas]: " << det << std::endl;
+    std::cout << "ret[lambda_i]: " << lambda_i << std::endl;
+    std::cout << "ret[lambda_g]: " << lambda_g << std::endl;
+    std::cout << "ret[weights]: " << weights << std::endl;
+    if (deterministics == "none") {
+        std::cout << "ret[y_d]: " << y_d << std::endl;
+        std::cout << "ret[My_d]: " << My_d << std::endl;
+        std::cout << "ret[v]: " << v << std::endl;
+        std::cout << "ret[Mv]: " << v << std::endl;
+    else {
+        std::cout << "ret[y_d]: " << y_d << std::endl;
+        std::cout << "ret[My_d]: " << y_d_old << std::endl;
+        std::cout << "ret[v]: " << v << std::endl;
+        std::cout << "ret[Mv]: " << v_old << std::endl;
+    }
+    std::cout << "ret[D]: " << D << std::endl;
 
     return ret;
 }
